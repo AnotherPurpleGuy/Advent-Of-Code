b73ed57282c12b621c76b0b5b428c6b5d35e9c43
AnotherPurpleGuy <32838564+AnotherPurpleGuy@users.noreply.github.com>
2023-04-24 18:42:10 +0100

    Day 1 Solution has passed eslinter

--- Day 1/Solution.js
+++ Day 1/Solution.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const { exit } = require('process')
 
 /*
-    This radixSort function will take and array of numbers and return it in descending 
+    This radixSort function will take and array of numbers and return it in descending
     order
 */
-function sortArr(arr) {
+function sortArr (arr) {
   const maxNum = Math.max(...arr)
   const maxNumDigits = Math.floor(Math.log10(maxNum)) + 1
 
@@ -21,25 +21,22 @@ function sortArr(arr) {
   return sortedArr
 }
 
-function getDigit(num, i) {
+function getDigit (num, i) {
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
 }
 
-/*  
-    This function will take the data array split it into each elf and then sum 
+/*
+    This function will take the data array split it into each elf and then sum
     the amount of calories each elf has. Once finished it will return an array of
     all the summed values.
 */
-function processData(data) {
+function processData (data) {
+  const dataArray = data.split('\r\n\r\n').map((element) => {
+    const subArray = element.split('\n')
+    const sum = subArray.reduce((acc, num) => acc + parseInt(num), 0)
 
-    const dataArray = data.split('\r\n\r\n').map((element) =>{
-
-        let subArray = element.split('\n')
-        let sum = subArray.reduce((acc, num) => acc + parseInt(num), 0)
-
-        return sum;
-
-    })
+    return sum
+  })
 
   return dataArray
 }
@@ -48,24 +45,18 @@ function processData(data) {
  * function will take a filename as a string as an argument and try and open it
  * as a 'utf-8' document. If there is an issue opening or reading the file the
  * error will be printed via 'console.error()' and the program will exit.
- * @param {string} filename 
+ * @param {string} filename
  * @returns contents of file as a string
  */
-function getData(filename) {
-  
+function getData (filename) {
   try {
-
-    let data = fs.readFileSync(filename,'utf-8')
+    const data = fs.readFileSync(filename, 'utf-8')
     return data
-
   } catch (err) {
-
     console.error('Error reading the file : ' + err.message)
     exit()
-
   }
 }
-
 
 /*
     This function will print the desired answer to the console.
@@ -73,10 +64,9 @@ function getData(filename) {
     the most calories we want to know the answer too. The orderedList
     is the ordered sum of all the elves
 */
-function printAnswer(numberOfElves, orderedList) {
-  
-  let tmp = orderedList.slice(0, numberOfElves)
-  let total = tmp.reduce((acc, num) => {
+function printAnswer (numberOfElves, orderedList) {
+  const tmp = orderedList.slice(0, numberOfElves)
+  const total = tmp.reduce((acc, num) => {
     return acc + num
   }, 0)
 
@@ -84,12 +74,10 @@ function printAnswer(numberOfElves, orderedList) {
   console.log('Sum of all these calories : ' + total)
 }
 
-
-
 // Entry point
 
 const rawData = getData('data.txt')
-var sums = processData(rawData)
+let sums = processData(rawData)
 
 sums = sortArr(sums)
 printAnswer(1, sums)

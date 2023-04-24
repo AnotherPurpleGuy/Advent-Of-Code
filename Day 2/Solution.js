2887f2474747b175f9545c54fc39abf919b2a050
AnotherPurpleGuy <32838564+AnotherPurpleGuy@users.noreply.github.com>
2023-04-25 00:10:29 +0100

    added standard getData function

--- Day 2/Solution.js
+++ Day 2/Solution.js
@@ -2,12 +2,19 @@ const { Worker, isMainThread, parentPort, workerData } = require('worker_threads
 const fs = require('fs')
 const { exit } = require('process')
 
+/**
+ * function will take a filename as a string as an argument and try and open it
+ * as a 'utf-8' document. If there is an issue opening or reading the file the
+ * error will be printed via 'console.error()' and the program will exit.
+ * @param {string} filename
+ * @returns contents of file as a string
+ */
 function getData (filename) {
   try {
     const data = fs.readFileSync(filename, 'utf-8')
     return data
   } catch (err) {
-    console.log('Error reading the file : ' + err.message)
+    console.error('Error reading the file : ' + err.message)
     exit()
   }
 }
